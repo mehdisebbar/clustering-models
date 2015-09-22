d1005e34c7aef6fd28d5e72876a1814c2cbcae51
mehdi <mehdi.sebbar.0@gmail.com>
2015-09-22 11:55:51 +0200

    wip

---
+++ graph_lassov2.py
@@ -0,0 +1,159 @@
+
+from sklearn.base import BaseEstimator
+from sklearn.utils import check_array
+from sklearn.cluster import KMeans
+import cvxpy as cvx
+import numpy as np
+from sklearn.mixture import GMM
+import scipy.stats
+
+class lassoEM(BaseEstimator):
+    """
+
+    """
+    def __init__(self, n_components=1, n_iter=1, lambd = None):
+        self.n_components = n_components
+        self.n_iter = n_iter
+        self.min_covar = 1e-3
+        if lambd == None:
+            self.alpha = [10 for _ in range(self.n_components)]
+        else:
+            self.alpha = lambd
+
+
+    def fit(self, X, y=None):
+
+        #Init step: (based on Kmeans)
+
+        X = check_array(X, dtype=np.float64)
+        if X.shape[0] < self.n_components:
+            raise ValueError(
+                'GMM estimation with %s components, but got only %s samples' %
+                (self.n_components, X.shape[0]))
+
+        max_log_prob = -np.infty
+        self.centers_ = KMeans(n_clusters= self.n_components, init="k-means++").fit(X).cluster_centers_
+        self.pi_ = np.tile(1.0 / self.n_components,self.n_components)
+
+        cv = np.linalg.inv(np.cov(X.T) + self.min_covar * np.eye(X.shape[1]))
+        if not cv.shape:
+            cv.shape = (1, 1)
+        self.omega_ = np.tile(cv, (self.n_components, 1, 1))
+
+        N = len(X)
+        Klist = range(self.n_components)
+        p = len(X[0])
+
+        current_log_likelihood = None
+
+        for i in range(self.n_iter):
+
+            prev_log_likelihood = current_log_likelihood
+            #Expectation step
+            tau = [[ self.tauik(x, k) for k in Klist] for x in X]
+
+            #Maximization step
+
+            rho = [[self.rhoik(tau, i, k) for k in Klist] for i in range(N)]
+
+            self.centers_ = [[sum([rho[i][k]*X[i][j] for i in range(N)])
+                              for j in range(len(X[0]))] for k in Klist]
+            self.pi_ = [sum( [taui[k] for taui in tau ])/len(X) for k in Klist]
+
+            sn = [[[sum([(X[i][j] - self.centers_[k][j]) * (X[i][l] - self.centers_[k][l]) * rho[i][k] for i in range(N)])
+                  for l in range(p)]
+                 for j in range(p)]
+                for k in Klist]
+
+            print "SN"
+            print sn
+
+            self.omega_ = [self.omega_solve(sn[k], self.alpha) for k in Klist]
+            print self.omega_
+
+
+    def tauik(self, x, k):
+        return (self.pi_[k]*gauss_dens_inv(self.centers_[k], self.omega_[k], x))/\
+               (sum([self.pi_[j]*gauss_dens_inv(self.centers_[j], self.omega_[j], x) for j in range(self.n_components)]))
+
+    def rhoik(self, tau, i, k):
+        return tau[i][k]/(sum([tauj[k] for tauj in tau]))
+
+    def omega_solve(self, snk, alpha):
+
+        # Create a variable that is constrained to the positive semidefinite cone.
+        n = len(snk[0])
+        S = cvx.semidefinite(n)
+
+    # Form the logdet(S) - tr(SY) objective. Note the use of a set
+    # comprehension to form a set of the diagonal elements of S*Y, and the
+    # native sum function, which is compatible with cvxpy, to compute the trace.
+    # TODO: If a cvxpy trace operator becomes available, use it!
+        obj = cvx.Minimize(-1/2*cvx.log_det(S) -1/2*sum([(S*np.array(snk))[i, i] for i in range(n)]))
+
+    # Set constraint.
+        constraints = [cvx.sum_entries(cvx.abs(S)) <= alpha]
+
+    # Form and solve optimization problem
+        prob = cvx.Problem(obj, constraints)
+        prob.solve()
+        if prob.status != cvx.OPTIMAL:
+            raise Exception('CVXPY Error')
+
+        # If the covariance matrix R is desired, here is how it to create it.
+
+        # Threshold S element values to enforce exact zeros:
+        S = S.value
+
+        S[abs(S) <= 1e-4] = 0
+        return np.array(S).tolist()
+
+    def loglikelihood(self, X, ):
+        pass
+
+def gauss_dens_inv(mu, omega, x):
+    return np.sqrt(np.linalg.det(omega) / ((2 * np.pi) ** len(x))) * np.exp(
+        -0.5 * np.dot(np.dot((x.T - mu), omega), (x.T - mu).T))
+
+def gaussM(pi, mus, sigmas, n):
+    pi = np.random.multinomial(n, pi, size=1)[0]
+    print "Repartition: ",pi
+    S = np.random.multivariate_normal(mus[0], sigmas[0], pi[0])
+    for idx, nk in enumerate(pi[1:]):
+        S = np.vstack([S, np.random.multivariate_normal(mus[idx+1], sigmas[idx+1], nk)])
+    np.random.shuffle(S)
+    return S
+
+if __name__ == '__main__':
+
+    weigths = [0.2, 0.5, 0.3]
+    centers = [[0, 0], [0, 3], [5, 0]]
+    vars = [[[3, 1],[1, 1]], [[3, 1],[1, 1]], [[4,0],[0,2]]]
+
+    X = gaussM(weigths,centers,vars, 1000)
+    lasso = lassoEM(n_components=3, n_iter=10, lambd= 0.10)
+    lasso.fit(X)
+
+    print "Lasso ==============="
+    print "means "
+    print lasso.centers_
+    print "covars "
+    print lasso.omega_
+    print "weights "
+    print lasso.pi_
+
+    cl = GMM(n_components=3, n_iter = 10,  covariance_type='diag')
+    cl.fit(X)
+    print "GMM ================"
+    print "weights "
+    print cl.weights_
+    print "means "
+    print cl.means_
+    print "covars "
+    print cl._get_covars()
+
+
+
+
+
+
--- graphlassoMain.py
+++ graphlassoMain.py
@@ -1,21 +1,33 @@
 from graph_lasso import graphlassoEM, gaussM
 from numpy.linalg import inv
+import numpy as np
+from sklearn.mixture import GMM
 
 if __name__ == '__main__':
-    mu1=[1,2]
-    mu2=[5,6]
-    sigma1= [[3,1],[1,3]]
-    sigma2= [[3,2],[2,6]]
-    mu=[mu1,mu2]
-    sigmas=[sigma1, sigma2]
-    X =gaussM([0.4, 0.6], mu, sigmas, 10)
-    om, muhat, pi = graphlassoEM(X, 2, [0.01,0.01])
-    print "real omega, ", inv(sigma1)
-    print "estimated omega, ", om[0]
-    print "real omega, ", inv(sigma2)
-    print "estimated omega, ", om[1]
-    print "real mu, ", mu1
-    print "estimated mu1, ",muhat[0]
-    print "real mu, ", mu2
-    print "estimated mu1, ",muhat[1]
+    mu1=[1,2,6]
+    mu2=[5,6,1]
+    mu3=[4,5,2]
+    sigma1= [[10,1,2],[1,2,4],[2,4,9]]
+    sigma2= [[3,2,5],[2,6,4],[5,4,10]]
+    sigma3= [[9,1,1],[1,4,1],[1,1,5]]
+    mu=[mu1,mu2, mu3]
+    sigmas=[sigma1, sigma2, sigma3]
+    X =gaussM([0.2, 0.3, 0.5], mu, sigmas, 200)
+    om, muhat, pi = graphlassoEM(X, 3, [20,20, 10])
+    print "real omega, \n", sigma1
+    print "real omega, \n", sigma2
+    print "real omega 3: \n", sigma3
+    print "----------------------------"
+    print "estimated omega, \n", om[1]
+    print "estimated omega, \n", om[0]
+    print 'estimated omega3: \n',om[2]
+    print "----------------------------"
+    print "real mu, \n", mu1
+    print "real mu, \n", mu2
+    print 'real mu:\n', mu3
+    print "----------------------------"
+    print "estimated mu3: \n", muhat[2]
+    print "estimated mu1, \n",muhat[0]
+    print "estimated mu1, \n",muhat[1]
+    print "----------------------------"
     print pi
