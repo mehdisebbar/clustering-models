ee0a24c36fa2cb043e7cbdcd36cc0a48d7274cb4
Mehdi Sebbar <mehdi.sebbar.0@gmail.com>
2017-09-26 12:29:16 +0200

    graph lasso

--- graph_lassov5.py
+++ graph_lassov5.py
@@ -1,18 +1,18 @@
-
+import numpy as np
+from numba import jit
 from sklearn.base import BaseEstimator
 from sklearn.utils import check_array
-import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.covariance import GraphLasso
-import rpy2.robjects.numpy2ri
-from rpy2.robjects.packages import importr
-rglasso = importr('glasso')
-rpy2.robjects.numpy2ri.activate()
-from numba import jit
+# TO USE GLASSO FROM THR R PACKAGE (Friedman et al.) uncomment:
+#import rpy2.robjects.numpy2ri
+#from rpy2.robjects.packages import importr
+#rglasso = importr('glasso')
+#rpy2.robjects.numpy2ri.activate()
 
 class GraphLassoMix(BaseEstimator):
     """
-
+    Graph lasso on GMM
     """
     def __init__(self, n_components=2, n_iter=20, alpha = None):
         self.n_components = n_components
@@ -22,20 +22,9 @@ class GraphLassoMix(BaseEstimator):
             self.alpha = [10 for _ in range(self.n_components)]
         else:
             self.alpha = alpha
-        self.model = [GraphLasso(alpha=self.alpha[k], assume_centered=True, tol=1e-4, verbose=True) for k in range(self.n_components)]
+        self.model = [GraphLasso(alpha=self.alpha[k], assume_centered=False, tol=1e-4) for k in range(self.n_components)]
 
     def fit(self, X, y=None):
-        """
-        :param X:
-        :param y:
-        :return:
-        """
-        X = check_array(X, dtype=np.float64)
-        if X.shape[0] < self.n_components:
-            raise ValueError(
-                'GMM estimation with %s components, but got only %s samples' %
-                 (self.n_components, X.shape[0]))
-
         #Init of params
         cv = np.linalg.inv(np.cov(X.T))
         if not cv.shape:
@@ -44,30 +33,20 @@ class GraphLassoMix(BaseEstimator):
         self.centers = KMeans(n_clusters= self.n_components, init="k-means++").fit(X).cluster_centers_
         self.pi = np.tile(1.0 / self.n_components,self.n_components)
         self.N, self.p = X.shape
-
         #EM-Iterations
         for i in range(self.n_iter):
-            #print "Beginning Step: ",i
-
-        #Expectation Step
+            #Expectation Step
             self.t = self.tau(X, self.N, self.p, self.n_components)
-
-        #Maximization Step
+            #Maximization Step
             self.pi = self.t.sum(axis=0)*1/self.N
             self.centers = np.array([(self.t[:,k]*X.T).sum(axis=1)*1/(self.N*self.pi[k]) for k in range(self.n_components)])
             #We normalize X with tau for sklearn graphlasso estimator
             Z = self.build_Z(X, self.N, self.p, self.centers, self.t, self.n_components)
             self.omegas = np.array([self.model[k].fit(Z[k]).precision_ for k in range(self.n_components)])
+            #Graph lasso code from R package (Friedman et al.)
             #self.omegas = np.array([ self.r_lasso_wrapper(Z[k]) for k in range(self.n_components)])
         self.clusters_assigned = self.t.argmax(axis = 1)
     
-    @jit
-    def cluster_assigned(self, X, N, p, n_components):
-        res = np.zeros([n_components, N, p])
-        for k in range(range(len(self.pi))):
-            res[k]=self.gauss_dens_inv_all(X, self.centers[k], self.omegas[k])
-        return res.argmax(axis = 1)
-
     @jit
     def build_Z(self, X, N, p, centers, t, n_components):
         res = np.zeros([n_components, N, p])
@@ -88,5 +67,5 @@ class GraphLassoMix(BaseEstimator):
         densities2 = densities.T*self.pi
         return (densities2.T/(densities2.sum(axis=1))).T
 
-    def r_lasso_wrapper(self, Z_k):
-        return np.array(rglasso.glasso(np.cov(Z_k.T),0.1)[1])
+    #def r_lasso_wrapper(self, Z_k):
+    #    return np.array(rglasso.glasso(np.cov(Z_k.T),0.1)[1])
